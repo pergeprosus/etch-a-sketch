96f4cd93052f8b3ac9f885c338ff6bea48b30493
Keith Floyd <keithfloyd2001@gmail.com>
2022-09-11 10:48:33 -0400

    Add grid loop in js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let container = document.getElementById("grid");
-let cell = document.createElement("div");
-cell.innerHTML = "ahh";
-container.appendChild(cell);
+let container = document.getElementById("container");
+//idea: make row and grid loops
+for (let i = 0; i < 6; i++) {
+    let cell = document.createElement("block");
+    let cells = document.getElementsByClassName("block")
+    cell.innerHTML = "O";
+    container.appendChild(cell);
+}
