427412acd0efb24d910ad322a5adcd4bb86d3976
Keith Floyd <keithfloyd2001@gmail.com>
2022-09-11 14:50:14 -0400

    Modify things for testing

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-let mainsize = 16;
+let mainsize = 256;
 //idea: make row and grid loops
 //vertical loop(?)
+
 let biggercontainer = document.getElementById("biggercontainer");
 for (let i = 0; i <= mainsize; i++) {
     let container = document.getElementById("rowcontainer");
     let cell = document.createElement("gridcell");
     let cell2 = document.createElement("gridcell");
-    let neurow = document.createElement("rowcontainer");
-    biggercontainer.appendChild(neurow);
-    //let allcell = document.getElementsByClassName("gridcell")
-    container.appendChild(cell);
-    neurow.appendChild(cell2);
+    container.appendChild(cell)
+    //nooprope.appendChild(cell);
+
 }
