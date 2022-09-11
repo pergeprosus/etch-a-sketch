6f57da39091a3732bd364ab1e4229f5c0fdbb089
Keith Floyd <keithfloyd2001@gmail.com>
2022-09-11 11:07:43 -0400

    Add CSS to header

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 //idea: make row and grid loops
 //vertical loop(?)
 for (let i = 0; i < 16; i++) {
+    let biggercontainer = document.getElementById("biggercontainer");
     let container = document.getElementById("container");
     let cell = document.createElement("gridcell");
     let allcell = document.getElementsByClassName("gridcell")
     cell.innerHTML = "O";
     container.appendChild(cell);
+    biggercontainer.appendChild(container);
+}
+
+for (let i =0; i < 16; i++) {
 }
