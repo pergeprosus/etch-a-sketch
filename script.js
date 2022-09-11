26081204ddb6d338f7a920a9652db7f18a08c5f0
Keith Floyd <keithfloyd2001@gmail.com>
2022-09-11 12:22:27 -0400

    Add skeleton for new rows

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 let mainsize = 16;
 //idea: make row and grid loops
 //vertical loop(?)
+let biggercontainer = document.getElementById("biggercontainer");
 for (let i = 0; i <= mainsize; i++) {
-    let biggercontainer = document.getElementById("biggercontainer");
-    let container = document.getElementById("container");
+    let container = document.getElementById("rowcontainer");
     let cell = document.createElement("gridcell");
-    let allcell = document.getElementsByClassName("gridcell")
-    cell.innerHTML = ""+ [i] +""
+    //let allcell = document.getElementsByClassName("gridcell")
     container.appendChild(cell);
-    biggercontainer.appendChild(container);
 }
-
-for (let i =0; i < 16; i++) {
+for (let i = 0; i <= mainsize; i++) {
+    let coontoon = document.createElement("rowcontainer");
+    biggercontainer.appendChild(coontoon);
 }
