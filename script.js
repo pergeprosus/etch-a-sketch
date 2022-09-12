e487bc0510584939cc1c7061211465c898a17343
Keith Floyd <keithfloyd2001@gmail.com>
2022-09-12 13:23:57 -0400

    Fix math for rowcontainer loop

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let mainsize = 16;
 //vertical loop(?)
 let smallercontainer = document.getElementById("smallercontainer");
 let biggercontainer = document.getElementById("biggercontainer");
-for (let i = 0; i <= mainsize; i++) {
+for (let i = 0; i <= mainsize - 1; i++) {
     let container = document.getElementById("rowcontainer");
     let cell = document.createElement("gridcell");
     container.appendChild(cell);
@@ -11,5 +11,7 @@ for (let i = 0; i <= mainsize; i++) {
 
 for (let i = 0; i <= mainsize; i++) {
     let fuck = document.createElement("rowcontainer");
+    
     smallercontainer.appendChild(fuck);
 }
+//Current goal: allow fullscale cells for each new one
