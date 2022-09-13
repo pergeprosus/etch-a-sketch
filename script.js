862c0d5a8d5cebd66f972ef4fe933d4c756da946
Keith Floyd <keithfloyd2001@gmail.com>
2022-09-13 17:02:18 -0400

    Implement new idea for universal drawing function

--- script.js
+++ script.js
@@ -5,20 +5,22 @@ let smallercontainer = document.getElementById("smallercontainer");
 let biggercontainer = document.getElementById("biggercontainer");
 let container = document.getElementById("rowcontainer");
 let gridcell = document.getElementById("gridcell");
-for (let i = 0; i <= mainsize - 1; i++) {
+let grids = document.querySelectorAll("gridcell");
+for (let q = 0; q <= mainsize - 1; q++) {
     let cell = document.createElement("gridcell");
     container.appendChild(cell);
+}
+for (let i = 0; i <= 1; i++) {
     let grids = document.querySelectorAll("gridcell");
     //First grid's mouseover loop thing
-    function clip() {
     grids[i].addEventListener('mouseover', function () {
         grids[i].style.backgroundColor = 'black';
-        console.log('touch')
     })
+    console.log('number of cells in a row that will be colored');
 }
-clip();
-    console.log('number of cells in a row');
-}
+
+
+
 
 //Clone containers
 for (let j = 0; j <= mainsize - 2; j++) {
@@ -27,11 +29,7 @@ for (let j = 0; j <= mainsize - 2; j++) {
     let containerclone = container.cloneNode(true);
     let gridclone = cell.cloneNode(true);
     smallercontainer.appendChild(containerclone);
-    let grids = document.querySelectorAll("gridcell");
-    //clone grid's mouseover loop thing
-
-
-
     console.log('number of clone rows (cells - 1)')
 }
 //current goal: Maybe add clone pixel to second loop and do the loop again difernt?
+//new idea
