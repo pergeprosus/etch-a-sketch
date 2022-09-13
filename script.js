ff9bb8043caa5d9b6f41c7f6e5d9ba83f5005915
Keith Floyd <keithfloyd2001@gmail.com>
2022-09-13 08:39:27 -0400

    Add event listener test code and console log for clones and cells being created in loop

--- script.js
+++ script.js
@@ -4,15 +4,27 @@ let mainsize = 16;
 let smallercontainer = document.getElementById("smallercontainer");
 let biggercontainer = document.getElementById("biggercontainer");
 let container = document.getElementById("rowcontainer");
+let gridcell = document.getElementById("gridcell");
+
 for (let i = 0; i <= mainsize - 1; i++) {
     let cell = document.createElement("gridcell");
     container.appendChild(cell);
+    console.log('cell is made')
+
 }
 //Clone containers
-for (let j = 0; j <= mainsize - 3; j++) {
+for (let j = 0; j <= mainsize - 2; j++) {
     let containerclone = container.cloneNode(true);
     smallercontainer.appendChild(containerclone);
+    console.log('clone is made')
 }
+document.querySelector('gridcell').addEventListener("click", flark);
+function flark() {
+    gridcell.style.backgroundColor = 'red';
+    return;
+}
+
+
 
 
 //Current goal: allow fullscale cells for each new one
