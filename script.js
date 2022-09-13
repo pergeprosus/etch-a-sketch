037be1ea117deab2e8703781d64cda6c9e1077c7
Keith Floyd <keithfloyd2001@gmail.com>
2022-09-13 11:30:26 -0400

    Add functioning draw tool for single cell

--- script.js
+++ script.js
@@ -9,22 +9,21 @@ let gridcell = document.getElementById("gridcell");
 for (let i = 0; i <= mainsize - 1; i++) {
     let cell = document.createElement("gridcell");
     container.appendChild(cell);
-    console.log('cell is made')
+    console.log('number of cells in a row');
 
 }
 //Clone containers
 for (let j = 0; j <= mainsize - 2; j++) {
     let containerclone = container.cloneNode(true);
     smallercontainer.appendChild(containerclone);
-    console.log('clone is made')
-}
-document.querySelector('gridcell').addEventListener("click", flark);
-function flark() {
-    gridcell.style.backgroundColor = 'red';
-    return;
+    console.log('number of clones (cells - 1)')
+
 }
 
+if ('gridcell') {
+document.querySelector('gridcell').addEventListener('mouseover', function() {
+document.querySelector('gridcell').style.backgroundColor = 'black'
+})
+}
 
-
-
-//Current goal: allow fullscale cells for each new one
+//Current goal: test functional event listen
