0dc618d74aac55fca4635384cfe9c51461c7a2bf
Keith Floyd <keithfloyd2001@gmail.com>
2022-09-13 15:01:54 -0400

    Wrap initial drawing function

--- script.js
+++ script.js
@@ -5,32 +5,33 @@ let smallercontainer = document.getElementById("smallercontainer");
 let biggercontainer = document.getElementById("biggercontainer");
 let container = document.getElementById("rowcontainer");
 let gridcell = document.getElementById("gridcell");
-
-
 for (let i = 0; i <= mainsize - 1; i++) {
     let cell = document.createElement("gridcell");
     container.appendChild(cell);
-    console.log('number of cells in a row');
     let grids = document.querySelectorAll("gridcell");
+    //First grid's mouseover loop thing
+    function clip() {
     grids[i].addEventListener('mouseover', function () {
         grids[i].style.backgroundColor = 'black';
         console.log('touch')
     })
-
-
 }
-
+clip();
+    console.log('number of cells in a row');
+}
 
 //Clone containers
 for (let j = 0; j <= mainsize - 2; j++) {
+    let gridcell = document.getElementById("gridcell");
+    let cell = document.createElement("gridcell");
     let containerclone = container.cloneNode(true);
+    let gridclone = cell.cloneNode(true);
     smallercontainer.appendChild(containerclone);
-    console.log('number of clones (cells - 1)')
-}
-//Beta whatever for drawing tool, currently only works on one dot
-let grids = document.querySelectorAll('gridcell');
-//document.querySelector('gridcell').addEventListener('mouseover', function() {
-  //  document.querySelector('gridcell').style.backgroundColor = 'black';
-//})
+    let grids = document.querySelectorAll("gridcell");
+    //clone grid's mouseover loop thing
 
-//Current goal: test functional event listen
+
+
+    console.log('number of clone rows (cells - 1)')
+}
+//current goal: Maybe add clone pixel to second loop and do the loop again difernt?
