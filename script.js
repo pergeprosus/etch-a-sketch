e2899d4e5b431ac6b124075e42aeb105736d5fb6
Keith Floyd <keithfloyd2001@gmail.com>
2022-09-14 17:54:50 -0400

    Combine color function loop with cell creation loop

--- script.js
+++ script.js
@@ -10,21 +10,19 @@ let grids = document.querySelectorAll("gridcell");
 for (let q = 0; q <= mainsize2; q++) {
     let cell = document.createElement("gridcell");
     container.appendChild(cell);
-}
-for (let i = 0; i <= (mainsize2); i++) {
     let grids = document.querySelectorAll("gridcell");
     //First grid's mouseover loop thing
-    grids[i].addEventListener('mouseover', function () {
-        grids[i].style.backgroundColor = 'black';
+    grids[q].addEventListener('mouseover', function () {
+        grids[q].style.backgroundColor = 'black';
+        console.log('touch')
     })
     console.log('number of cells in a row that will be colored');
 }
-//Clone containers
 for (let j = 0; j <= mainsize2 - 1; j++) {
     let cell = document.createElement("gridcell");
     let containerclone = container.cloneNode(true);
     smallercontainer.appendChild(containerclone);
     console.log('number of clone rows (cells - 1)')
 }
-//current goal: Maybe add clone pixel to second loop and do the loop again difernt?
-//new idea
+console.log(grids.length)
+
