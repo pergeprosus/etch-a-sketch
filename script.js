a9644e3196a3ae234bf1db496f11fad1147c37bd
Keith Floyd <keithfloyd2001@gmail.com>
2022-09-15 13:46:45 -0400

    Create and implement removeall function

--- script.js
+++ script.js
@@ -6,40 +6,65 @@ let container = document.getElementById("rowcontainer");
 let gridcell = document.getElementById("gridcell");
 let grids = document.querySelectorAll("gridcell");
 let button = document.getElementById("sizerequest")
+let sizecounter = document.getElementById("thesize")
 
-function gridcellcreation() {
-for (let q = 0; q <= mainsize2; q++) {
-    let cell = document.createElement("gridcell");
-    container.appendChild(cell);
-    let grids = document.querySelectorAll("gridcell");
-    console.log('number of cells in a row that will be colored');
+function sizecounteration() {
+    sizecounter.innerHTML = "" + mainsize + " x " + mainsize + "";
 }
+sizecounteration();
+function gridcellcreation() {
+    for (let q = 0; q <= mainsize2; q++) {
+        let cell = document.createElement("gridcell");
+        container.appendChild(cell);
+        let grids = document.querySelectorAll("gridcell");
+        console.log('number of cells in a row that will be colored');
+    }
 }
 gridcellcreation();
-
-function clonerowcreation () {
-for (let j = 0; j <= mainsize2 - 1; j++) {
-    let cell = document.createElement("gridcell");
-    let containerclone = container.cloneNode(true);
-    smallercontainer.appendChild(containerclone);
-    console.log('number of clone rows (cells - 1)')
-}
+function clonerowcreation() {
+    for (let j = 0; j <= mainsize2 - 1; j++) {
+        let cell = document.createElement("gridcell");
+        let containerclone = container.cloneNode(true);
+        smallercontainer.appendChild(containerclone);
+        console.log('number of clone rows (cells - 1)')
+    }
 }
 clonerowcreation();
-
-function colorenabler () {
-let zingle = document.getElementsByTagName('gridcell');
-for (let z = 0; z < zingle.length; z++) {
-    zingle[z].addEventListener('mouseover', function () {
-        zingle[z].style.backgroundColor = 'black';
-    })
-}
+function colorenabler() {
+    let zingle = document.getElementsByTagName('gridcell');
+    for (let z = 0; z < zingle.length; z++) {
+        zingle[z].addEventListener('mouseover', function () {
+            zingle[z].style.backgroundColor = 'black';
+        })
+    }
 }
 colorenabler();
+
+function scaler() { 
+    let container = document.getElementById('rowcontainer'); 
+    let containers = document.getElementsByTagName('rowcontainer')
+    let elements = container.getElementsByTagName('gridcell'); 
+    let smallercontainer = document.getElementById("smallercontainer");
+    let smalcontainlength = document.getElementById("smallercontainer").childElementCount;
+    for (let i = 0; i < elements.length; i++) { 
+        let element = elements[i]; 
+        elements.style.transform ="scale(" + (1 / elements.length) + ")"; 
+        elements.style.backgroundColor = "white";
+    } 
+}
+let elements = container.getElementsByTagName('gridcell');
+
+function removeall() {
+    for (let i = 0; i < mainsize2 + 1; i++) {
+    let smallcontainer = document.getElementById('smallercontainer');
+    let containers = document.getElementsByTagName('rowcontainer')
+    smallcontainer.removeChild(smallcontainer.lastChild);
+    smallcontainer.removeChild(smallcontainer.lastChild);
+}
+}
 
 //Current goal: Allow for dynamic box sizes
 button.addEventListener('click', function () {
     console.log('button press')
-    mainsize2 = 32;
-    gridcellcreation();
+    removeall();
 })
